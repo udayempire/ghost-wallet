a9638b02bd1440db348dbf51280e7678e859adf4
www.udayempire <147295044+udayempire@users.noreply.github.com>
2025-03-29 02:07:55 +0530

    removed unnecessary stuffs

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,9 @@ import { ChooseNetwork } from "@/components/ChooseNetwork";
 import { ExampleSeed } from "@/components/ExampleSeed";
 import { FAQs } from "@/components/FAQs";
 import { Footer } from "@/components/Footer";
-import { PulsatingButton } from "@/components/magicui/pulsating-button";
 import { WalletCreationDetails } from "@/components/WalletCreationDetails";
 import { useRouter } from "next/navigation"
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   const router = useRouter();
   return (
@@ -40,19 +39,21 @@ export default function Home() {
                 send transactions effortlessly.
               </p>
               <div className="flex flex-col md:flex-row  gap-8 mt-10">
-                <PulsatingButton
+                <Button
                   onClick={() => {
                     router.push("/#selectNetwork")
                   }}
+                  className="bg-zinc-100 px-6 py-6 text-md text-black rounded-sm hover:bg-zinc-300 cursor-pointer"
 
-                >Create New Wallet</PulsatingButton>
-                <PulsatingButton
+                >Create New Wallet</Button>
+                <Button
                   onClick={() => {
                     router.push("/import-wallet")
                   }}
-                >Import Existing Wallet</PulsatingButton>
+                  className="bg-zinc-100 p-6 text-black text-md rounded-sm hover:bg-zinc-300 cursor-pointer"
+                >Import Existing Wallet</Button>
                 {/* TO DO - Create a button component and add href(optional) into it and remove use client in this page */}
-                <PulsatingButton onClick={()=>{router.push("/check-wallet-balance?network=ethereum")}}>Check Crypto Balances</PulsatingButton>
+                <Button onClick={()=>{router.push("/check-wallet-balance?network=ethereum")}} className="bg-zinc-100 p-6 text-md text-black rounded-sm hover:bg-zinc-300 cursor-pointer">Check Crypto Balances</Button>
               </div>
             </div>
             <div>

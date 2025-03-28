e3331b3653de6e26428a002b0fd098865438d076
www.udayempire <147295044+udayempire@users.noreply.github.com>
2025-03-29 00:04:13 +0530

    removed suspense issue

--- app/check-wallet-balance/page.tsx
+++ app/check-wallet-balance/page.tsx
@@ -1,7 +1,11 @@
+import { Suspense } from "react";
 import ClientCheckAccount from "./ClientCheckAccount";
 
-export default function checkWalletBalance(){
-    return <div>
-        <ClientCheckAccount/>
-    </div>
+export default function checkWalletBalance() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <ClientCheckAccount />
+        </Suspense>
+    )
+
 }

17a6a404e8217a6c88609cf32316fe1f64ed4b07
www.udayempire <147295044+udayempire@users.noreply.github.com>
2025-03-29 01:01:16 +0530

    added logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { geistVF } from "./fonts";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner" 
+import { Toaster } from "@/components/ui/sonner"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,11 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon_io/favicon.ico" sizes="any" />
       <body
         className={`${geistSans.variable} ${geistVF.className} antialiased`}
       >
         {children}
-        <Toaster/>
+        <Toaster />
       </body>
     </html>
   );
